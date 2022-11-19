a97578c9d0945df5f798f8ac8d38454268df16c1
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-11-19 14:22:12 +0100

    error handling

--- frontend/src/components/Ballot.tsx
+++ frontend/src/components/Ballot.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { checkBlockchainConnection, getAllCandidates, castVote } from '../ether'
 import Row from './Row'
 import '../styles/ballot.css'
+import { Alert } from 'react-bootstrap'
 
 interface props {
   setStep: React.Dispatch<React.SetStateAction<number>>
@@ -12,6 +13,7 @@ interface props {
 function Ballot({ setStep, setVoteCasted, setVoteProcessed }: props) {
   const [selectedCandidate, setSelectedCandidate] = useState(-1)
   const [candidates, setCandidates] = useState<string[]>([])
+  const [error, setError] = useState('')
 
   const checkboxHandle = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSelectedCandidate(parseInt(event.target.value))
@@ -26,13 +28,27 @@ function Ballot({ setStep, setVoteCasted, setVoteProcessed }: props) {
 
   const initializeBallot = () => {
     checkBlockchainConnection()
-      ? getAllCandidates().then((res) => setCandidates(res))
-      : alert('install metamask')
+      ? getAllCandidates(setError)
+          .then((res) => setCandidates(res))
+          .catch((res) =>
+            setError(
+              'Make sure you are on Mumbai test network and refresh the page',
+            ),
+          )
+      : setError(
+          'Install Metamast and create an account before using the application',
+        )
   }
 
   // Increment voting step and cast vote
   const handleVoteButton = () => {
     castVote(selectedCandidate, setVoteCasted, setVoteProcessed)
+      .then()
+      .catch(() =>
+        setError(
+          'Make sure you are on Mumbai test network and refresh the page',
+        ),
+      )
   }
 
   // Fetch candidates
@@ -42,7 +58,10 @@ function Ballot({ setStep, setVoteCasted, setVoteProcessed }: props) {
 
   return (
     <>
-      {candidates.length > 0 ? (
+      <Alert variant="danger" show={error !== ''}>
+        {error}
+      </Alert>
+      {candidates?.length > 0 ? (
         <div className="ballot">
           {candidates.map((can, index) => (
             <Row checkboxHandle={checkboxHandle} value={index} name={can} />
@@ -56,7 +75,7 @@ function Ballot({ setStep, setVoteCasted, setVoteProcessed }: props) {
           </button>
         </div>
       ) : (
-        <span>Loading...</span>
+        error === '' && <span>Loading...</span>
       )}
     </>
   )
