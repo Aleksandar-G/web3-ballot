3b168019f97cdd23b402e81f39de4a195c0603b7
Aleksandar-G <72451741+Aleksandar-G@users.noreply.github.com>
2022-11-24 12:10:22 +0100

    add piechart for votes

--- frontend/src/components/ProcessVote.tsx
+++ frontend/src/components/ProcessVote.tsx
@@ -1,25 +1,56 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Alert from 'react-bootstrap/Alert'
+import Modal from 'react-bootstrap/Modal'
+import { PieChart } from './PieChart'
 
 interface props {
   voteProcessed: boolean
 }
 
 export const ProcessVote = ({ voteProcessed }: props) => {
-  useEffect(() => {
-    console.log(voteProcessed)
-  }, [voteProcessed])
+  const [showModal, setShowModal] = useState(false)
+
+  const handleCloseModal = () => setShowModal(false)
+  const handleShowModal = () => setShowModal(true)
 
   return (
-    <div>
-      {voteProcessed ? (
-        <Alert variant="success">Vote processed CONGRATS</Alert>
-      ) : (
-        <Alert variant="warning">vote is being proceessed...</Alert>
-      )}
+    <>
+      <div>
+        {voteProcessed ? (
+          <Alert className="w-75 mx-auto mt-3" variant="success">
+            You have volted
+          </Alert>
+        ) : (
+          <Alert className="w-75 mx-auto mt-3" variant="warning">
+            Your vote is being proceessed...
+          </Alert>
+        )}
+      </div>
+      <div className="mx-auto w-25">
+        <Button
+          onClick={() => {
+            handleShowModal()
+          }}
+          variant="primary"
+        >
+          View Graph
+        </Button>
+      </div>
 
-      <Button variant="primary">View Graph</Button>
-    </div>
+      <Modal
+        show={showModal}
+        onHide={handleCloseModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Vote distribution</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <PieChart />
+        </Modal.Body>
+      </Modal>
+    </>
   )
 }
